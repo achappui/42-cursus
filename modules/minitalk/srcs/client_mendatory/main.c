753d78b7223e5289eb2384caaa5a02dfba07431b
testpatrick555 <letestpoursteamatk@gmail.com>
2024-01-02 14:10:40 +0100

    tocheckminitalk

--- modules/minitalk/srcs/client_mendatory/main.c
+++ modules/minitalk/srcs/client_mendatory/main.c
@@ -6,24 +6,38 @@
 /*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:21:24 by achappui          #+#    #+#             */
-/*   Updated: 2024/01/02 12:51:49 by achappui         ###   ########.fr       */
+/*   Updated: 2024/01/02 14:10:19 by achappui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-char	*g_packet;
+char	*g_packet = NULL;
 
-void	prepare_packet(char *str)
+static void	end_process(char error)
+{
+	free(g_packet);
+	g_packet = NULL;
+	if (error)
+		ft_printf("[SERVER] Message could not be delivered ! \
+					(malloc error)\n");
+	else
+		ft_printf("[SERVER] Message was successfully delivered !\n");
+	exit(0);
+}
+
+static void	prepare_packet(char *str)
 {
 	unsigned int	len;
 	unsigned int	i;
 
 	len = (unsigned int)ft_strlen(str);
-	ft_printf("STRING LEN: %u\n", len);
 	g_packet = (char *)malloc((ft_strlen(str) + sizeof(int)) * sizeof(char));
 	if (!g_packet)
-		exit(write(2, "[CLIENT] MALLOC ERROR !\n", 24));
+	{
+		ft_printf("[CLIENT] Message could not be sent ! (malloc error)\n");
+		exit(1);
+	}
 	g_packet[0] = (char)(len >> 24);
 	g_packet[1] = (char)(len >> 16);
 	g_packet[2] = (char)(len >> 8);
@@ -39,7 +53,7 @@ void	prepare_packet(char *str)
 	g_packet -= 4;
 }
 
-void	packet_sender(int sig, siginfo_t *info, void *ptr)
+static void	packet_sender(int sig, siginfo_t *info, void *ptr)
 {
 	static unsigned char	mask = 0;
 	static unsigned int		i = UINT_MAX;
@@ -51,11 +65,8 @@ void	packet_sender(int sig, siginfo_t *info, void *ptr)
 	{
 		mask = 1 << 7;
 		i++;
-		if (i > 3 && g_packet[i] == '\0')
-		{
-			//ft_printf("[CLIENT] Message was sent !\n");
-			exit(0);
-		}
+		if (sig == SIGUSR2)
+			end_process(!(i > 3 && g_packet[i] == '\0'));
 	}
 	if (g_packet[i] & mask)
 		kill(info->si_pid, SIGUSR1);
@@ -71,9 +82,11 @@ int	main(int argc, char *argv[])
 	sa.sa_flags = SA_RESTART | SA_SIGINFO;
 	sa.sa_sigaction = &packet_sender;
 	sigaction(SIGUSR1, &sa, NULL);
-	//ft_printf("[CLIENT] %d\n", getpid());
+	sigaction(SIGUSR2, &sa, NULL);
 	prepare_packet(argv[2]);
 	kill(ft_atoi(argv[1]), SIGUSR1);
+	if ()
+	ft_printf("[CLIENT] Could not reach the server !\n");
 	while (1)
 		pause();
 	return (0);
--- modules/minitalk/srcs/server_mendatory/main.c
+++ modules/minitalk/srcs/server_mendatory/main.c
@@ -6,17 +6,17 @@
 /*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 13:29:29 by achappui          #+#    #+#             */
-/*   Updated: 2024/01/02 12:52:45 by achappui         ###   ########.fr       */
+/*   Updated: 2024/01/02 13:56:23 by achappui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void	getting_len(int sig, unsigned int *len, char **str, char *state)
+void	getting_len(siginfo_t *info, unsigned int *len, char **str, char *state)
 {
 	static unsigned int	mask1 = 1U << 31U;
 
-	if (sig == SIGUSR1)
+	if (info->si_signo == SIGUSR1)
 		*len |= mask1;
 	mask1 /= 2;
 	if (mask1 == 0)
@@ -24,7 +24,11 @@ void	getting_len(int sig, unsigned int *len, char **str, char *state)
 		mask1 = 1U << 31U;
 		*str = (char *)ft_calloc(*len + 1, sizeof(char));
 		if (!*str)
-			exit(write(2, "[SERVER] MALLOC ERROR !\n", 24));
+		{
+			kill(info->si_pid, SIGUSR2);
+			ft_printf("[SERVER] MALLOC ERROR\n");
+			exit(1);
+		}
 		*state = GETTING_STR;
 	}
 }
@@ -49,15 +53,15 @@ void	getting_str(int sig, unsigned int *len, char **str, char *state)
 	}
 }
 
-void	end_process(char **str, unsigned int *len, char *state)
+void	end_process(pid_t client_pid, char **str, unsigned int *len, char *state)
 {
 	write(1, *str, *len);
-	ft_printf(" LEN: %u", *len);
 	write(1, "\n", 1);
 	free(*str);
 	*str = NULL;
 	*len = 0;
 	*state = ASK_FIRST_BIT;
+	kill(client_pid, SIGUSR2);
 }
 
 void	handle_sigusr(int sig, siginfo_t *info, void *ptr)
@@ -70,11 +74,11 @@ void	handle_sigusr(int sig, siginfo_t *info, void *ptr)
 	if (state == ASK_FIRST_BIT)
 		state = GETTING_LEN;
 	else if (state == GETTING_LEN)
-		getting_len(sig, &len, &str, &state);
+		getting_len(info, &len, &str, &state);
 	else if (state == GETTING_STR)
 		getting_str(sig, &len, &str, &state);
 	if (state == PROCESS_END)
-		end_process(&str, &len, &state);
+		end_process(info->si_pid, &str, &len, &state);
 	kill(info->si_pid, SIGUSR1);
 }
 
@@ -87,7 +91,6 @@ int	main(void)
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("[SERVER] %d\n", getpid());
-	ft_printf("[SERVER] Waiting for signal...\n");
 	while (1)
 		pause();
 	return (0);
